1dde1081a41847c239a84d71184ba04923ed577a
Rahul Thakur <rahulthakur.developer@gmail.com>
2025-03-31 18:54:58 +0530

    populate airplaneDetails and airportDetails from Flight Search API

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -1,5 +1,6 @@
 const CrudRepository = require('./crud-repository');
-const { Flight } = require('../models');
+const { Flight, Airport, Airplane, City } = require('../models');
+const { Sequelize } = require('sequelize');
 
 
 class FlightRepository extends CrudRepository {
@@ -11,10 +12,43 @@ class FlightRepository extends CrudRepository {
         const response = await Flight.findAll({
             where: filter,
             order: sort,
+            include: [
+                { 
+                    model: Airplane,
+                    required: true, 
+                    as: 'airplaneDetail',
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'departureAirport',
+                    on:{
+                        col1: Sequelize.where(Sequelize.col('Flight.departureAirportId'), "="  ,Sequelize.col('departureAirport.code')),
+                    },
+                    include: {
+                        model: City,
+                        required: true
+                    }
+
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'arrivalAirport',
+                    on:{
+                        col1: Sequelize.where(Sequelize.col('Flight.arrivalAirportId'), "="  ,Sequelize.col('arrivalAirport.code')),
+                    },
+                    include: {
+                        model: City,
+                        required: true
+                    }
+                },
+
+            ]
         });
         return response;
     }
 
 }
 
-module.exports = FlightRepository;
+module.exports = FlightRepository
